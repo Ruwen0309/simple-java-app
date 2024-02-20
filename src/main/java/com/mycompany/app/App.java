e6ee5079bb28004d1784b795397b327902394b31
Ru Wen <mokruwen@gmail.com>
2024-02-20 16:34:22 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is fantastic");
+        System.out.println("Changing on demo periodic polling");
     }
 
     public String getMessage() {
